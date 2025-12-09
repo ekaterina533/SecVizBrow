a76eefe37c7ea9d2421584a7ed9ded8620a378ff
ekaterina533 <80419086+ekaterina533@users.noreply.github.com>
2025-12-09 22:05:49 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -6,12 +6,12 @@ load_dotenv()
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'super-secret-key')
-    DB_USER = os.getenv('DB_USER', 'admin')
-    DB_PASSWORD = os.getenv('DB_PASSWORD', 'SecurePass210411!')
-    DB_HOST = os.getenv('DB_HOST', 'localhost')
-    DB_PORT = os.getenv('DB_PORT', '3306')
-    DB_NAME = os.getenv('DB_NAME', 'secviz')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_PORT = os.getenv('DB_PORT')
+    DB_NAME = os.getenv('DB_NAME')
     
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
